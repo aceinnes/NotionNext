755c1efa8087f4fdbe55c8eba94bb598b3c54653
Ace <125452471+aceinnes@users.noreply.github.com>
2024-08-21 21:42:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'XZDDF', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '七年公众号作者，数字游民，野生开发者', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'www.xzddf.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.xzddf.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'XZDDF, 写字的地方', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
